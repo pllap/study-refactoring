f6d0f7c750cc9744499eeeb883ba4bafebd19d7f
권혁우 <pllapallpal@gmail.com>
2025-01-15 12:26:42 +0900

    Ch1: Extract method `totalAmountFor()`

--- src/main/kotlin/chapter1/Statement.kt
+++ src/main/kotlin/chapter1/Statement.kt
@@ -48,7 +48,16 @@ fun statement(invoice: Invoice, plays: Map<String, Play>): String {
         return result
     }
 
-    var totalAmount = 0
+    fun totalAmountFor(performances: List<Invoice.Performance>): Int {
+        var result = 0
+
+        for (performance in performances) {
+            result += amountFor(performance)
+        }
+
+        return result
+    }
+
     var result = "청구 내역 (고객명: ${invoice.customer})\n"
     val format = NumberFormat.getCurrencyInstance(Locale.US).also {
         it.maximumFractionDigits = 2
@@ -58,10 +67,9 @@ fun statement(invoice: Invoice, plays: Map<String, Play>): String {
     for (performance in invoice.performances) {
         // 청구 내역을 출력한다.
         result += "  ${playFor(performance).name}: ${format.format(amountFor(performance) / 100.0)} (${performance.audience}석)\n"
-        totalAmount += amountFor(performance)
     }
 
-    result += "총액: ${format.format(totalAmount / 100.0)}\n"
+    result += "총액: ${format.format(totalAmountFor(invoice.performances) / 100.0)}\n"
     result += "적립 포인트: ${volumeCreditsFor(invoice.performances)}점\n"
     return result
 }
