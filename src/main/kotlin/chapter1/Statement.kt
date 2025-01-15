b3e5a16ff2db1baf7439c0826b54de704d7609f9
권혁우 <pllapallpal@gmail.com>
2025-01-15 22:01:04 +0900

    Ch1: Extract `renderPlainText()`

--- src/main/kotlin/chapter1/Statement.kt
+++ src/main/kotlin/chapter1/Statement.kt
@@ -4,6 +4,10 @@ import java.text.NumberFormat
 import java.util.*
 
 fun statement(invoice: Invoice, plays: Map<String, Play>): String {
+    return renderPlainText(invoice, plays)
+}
+
+fun renderPlainText(invoice: Invoice, plays: Map<String, Play>): String {
     fun playFor(performance: Invoice.Performance): Play {
         return plays[performance.playID] ?: throw RuntimeException("알 수 없는 장르: ${performance.playID}")
     }
