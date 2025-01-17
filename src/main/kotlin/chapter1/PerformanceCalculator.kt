bf08938760dc3984db17ff2661d9f79b0695059d
권혁우 <pllapallpal@gmail.com>
2025-01-17 23:08:52 +0900

    Ch1: Remove `amountFor()`, `volumeCreditsFor()`, calculate totalAmount and totalVolumeCredits using calculator

--- src/main/kotlin/chapter1/PerformanceCalculator.kt
+++ src/main/kotlin/chapter1/PerformanceCalculator.kt
@@ -50,51 +50,15 @@ fun createStatementData(invoice: Invoice, plays: Map<String, Play>): StatementDa
         return plays[performance.playID] ?: throw RuntimeException("알 수 없는 장르: ${performance.playID}")
     }
 
-    fun amountFor(performance: Invoice.Performance): Int {
-        var result: Int
-
-        when (playFor(performance).type) {
-            "tragedy" -> {
-                result = 40000
-                if (performance.audience > 30) {
-                    result += 1000 * (performance.audience - 30)
-                }
-            }
-
-            "comedy" -> {
-                result = 30000
-                if (performance.audience > 20) {
-                    result += 10000 + 500 * (performance.audience - 20)
-                }
-                result += 300 * performance.audience
-            }
-
-            else -> throw RuntimeException("알 수 없는 장르: ${playFor(performance).type}")
-        }
-
-        return result
-    }
-
-    fun volumeCreditsFor(performance: Invoice.Performance): Int {
-        var result = 0
-        // 포인트를 적립한다.
-        result += maxOf(performance.audience - 30, 0)
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" == playFor(performance).type) {
-            result += performance.audience / 5
-        }
-        return result
-    }
-
     fun totalAmount(performances: List<Invoice.Performance>): Int {
         return performances
-            .map { amountFor(it) }
+            .map { PerformanceCalculator(it, playFor(it)).amount }
             .reduce { total, amount -> total + amount }
     }
 
     fun totalVolumeCredits(performances: List<Invoice.Performance>): Int {
         return performances
-            .map { volumeCreditsFor(it) }
+            .map { PerformanceCalculator(it, playFor(it)).volumeCredits }
             .reduce { total, volumeCredits -> total + volumeCredits }
     }
 
