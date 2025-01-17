7712f137b9452b239992f0f9dab72f50ef456e7c
권혁우 <pllapallpal@gmail.com>
2025-01-17 22:42:13 +0900

    Ch1: Extract method `createStatementData()`

--- src/main/kotlin/chapter1/Statement.kt
+++ src/main/kotlin/chapter1/Statement.kt
@@ -4,73 +4,75 @@ import java.text.NumberFormat
 import java.util.*
 
 fun statement(invoice: Invoice, plays: Map<String, Play>): String {
-    fun playFor(performance: Invoice.Performance): Play {
-        return plays[performance.playID] ?: throw RuntimeException("알 수 없는 장르: ${performance.playID}")
-    }
-
-    fun amountFor(performance: Invoice.Performance): Int {
-        var result: Int
-
-        when (playFor(performance).type) {
-            "tragedy" -> {
-                result = 40000
-                if (performance.audience > 30) {
-                    result += 1000 * (performance.audience - 30)
-                }
-            }
-
-            "comedy" -> {
-                result = 30000
-                if (performance.audience > 20) {
-                    result += 10000 + 500 * (performance.audience - 20)
-                }
-                result += 300 * performance.audience
-            }
-
-            else -> throw RuntimeException("알 수 없는 장르: ${playFor(performance).type}")
+    fun createStatementData(): StatementData {
+        fun playFor(performance: Invoice.Performance): Play {
+            return plays[performance.playID] ?: throw RuntimeException("알 수 없는 장르: ${performance.playID}")
         }
 
-        return result
-    }
+        fun amountFor(performance: Invoice.Performance): Int {
+            var result: Int
 
-    fun volumeCreditsFor(performance: Invoice.Performance): Int {
-        var result = 0
-        // 포인트를 적립한다.
-        result += maxOf(performance.audience - 30, 0)
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" == playFor(performance).type) {
-            result += performance.audience / 5
+            when (playFor(performance).type) {
+                "tragedy" -> {
+                    result = 40000
+                    if (performance.audience > 30) {
+                        result += 1000 * (performance.audience - 30)
+                    }
+                }
+
+                "comedy" -> {
+                    result = 30000
+                    if (performance.audience > 20) {
+                        result += 10000 + 500 * (performance.audience - 20)
+                    }
+                    result += 300 * performance.audience
+                }
+
+                else -> throw RuntimeException("알 수 없는 장르: ${playFor(performance).type}")
+            }
+
+            return result
         }
-        return result
+
+        fun volumeCreditsFor(performance: Invoice.Performance): Int {
+            var result = 0
+            // 포인트를 적립한다.
+            result += maxOf(performance.audience - 30, 0)
+            // 희극 관객 5명마다 추가 포인트를 제공한다.
+            if ("comedy" == playFor(performance).type) {
+                result += performance.audience / 5
+            }
+            return result
+        }
+
+        fun totalAmount(performances: List<Invoice.Performance>): Int {
+            return performances
+                .map { amountFor(it) }
+                .reduce { total, amount -> total + amount }
+        }
+
+        fun totalVolumeCredits(performances: List<Invoice.Performance>): Int {
+            return performances
+                .map { volumeCreditsFor(it) }
+                .reduce { total, volumeCredits -> total + volumeCredits }
+        }
+
+        return StatementData(
+            customer = invoice.customer,
+            performances = invoice.performances.map {
+                StatementData.Performance(
+                    play = playFor(it),
+                    audience = it.audience,
+                    amount = amountFor(it),
+                    volumeCredits = volumeCreditsFor(it),
+                )
+            },
+            totalAmount = totalAmount(invoice.performances),
+            totalVolumeCredits = totalVolumeCredits(invoice.performances),
+        )
     }
 
-    fun totalAmount(performances: List<Invoice.Performance>): Int {
-        return performances
-            .map { amountFor(it) }
-            .reduce { total, amount -> total + amount }
-    }
-
-    fun totalVolumeCredits(performances: List<Invoice.Performance>): Int {
-        return performances
-            .map { volumeCreditsFor(it) }
-            .reduce { total, volumeCredits -> total + volumeCredits }
-    }
-
-    val statementData = StatementData(
-        customer = invoice.customer,
-        performances = invoice.performances.map {
-            StatementData.Performance(
-                play = playFor(it),
-                audience = it.audience,
-                amount = amountFor(it),
-                volumeCredits = volumeCreditsFor(it),
-            )
-        },
-        totalAmount = totalAmount(invoice.performances),
-        totalVolumeCredits = totalVolumeCredits(invoice.performances),
-    )
-
-    return renderPlainText(statementData)
+    return renderPlainText(createStatementData())
 }
 
 fun renderPlainText(statementData: StatementData): String {
